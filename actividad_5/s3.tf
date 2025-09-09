34ead599d09a446e3bbe6e07209f095684622a74
Iker Deras <ikerrekiu@outlook.com>
2025-09-09 23:57:43 +0000

    feat: mover y corregir s3.tf a actividad_5/

---
+++ actividad_5/s3.tf
@@ -0,0 +1,14 @@
+resource "random_id" "bucket"_suffix" {
+    byte_length = 7 
+}
+
+resource "aws_s3_bucket" "iker-adrian_bucket" {
+    bucket = "iker-adrian-bucket-%{random_id-bucket_suffix.hex}"
+
+}
+
+output "bucket_name" {
+    value = aws_s3_bucket.iker-adrian_bucket.bucket
+}
+
+
