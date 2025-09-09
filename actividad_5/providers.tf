c621032ab4c39a03e648c9ab93a92020a1d51097
Iker Deras <ikerrekiu@outlook.com>
2025-09-09 22:58:42 +0000

    fix: corregir providers.tf (backend y providers)

--- actividad_5/providers.tf
+++ actividad_5/providers.tf
@@ -7,12 +7,22 @@ terraform {
 		}
 		random = {
 			source  = "hashicorp/random"
-			version = ">= 3.6"
+			version = ">= 3.0"
 		}
+	}
+
+	# IMPORTANTE: El bucket indicado en el backend debe existir previamente.
+	# No intentes crearlo en el mismo plan que lo usa como backend.
+	backend "s3" {
+		bucket = "tf-remote-backend-0786" # eliminar espacio inicial
+		key    = "state/actividad_5/terraform.tfstate"
+		region = "us-east-2"
+		encrypt = true
 	}
 }
 
 provider "aws" {
 	region = var.region
 }
+
 
